c05826670d73414fffa7424e2df4c0f306f219b5
Chris Roberts <code@chrisroberts.org>
2015-03-31 09:27:46 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,141 @@
+# Infrastructure tooling
+
+This document defines configuration of a local *nix workstation to extend
+bash functionality in aiding common infrastructure configuration issues.
+It has builtin helpers for dealing with multiple cloud provider account
+secrets as well as Chef specific configuration helpers.
+
+## Setup
+
+### Install tools
+
+To start, the required collection of tools must be installed. To prepare the
+system, create `bin` directory in the home directory:
+
+```
+$ mkdir ~/bin
+```
+
+Next, install the tools
+
+* rvm (optional)
+* hub -> into ~/bin
+* direnv -> into ~/bin
+* bash-it -> clone to .bash-it
+* infra bash-it plugin -> copy to plugins
+
+### Configure bash
+
+Bash must be configured to use the (bash-it)[1] library. Start by running the
+install script:
+
+```
+$ ~/.bash_it/install.sh
+```
+
+Now check the `~/.bash_profile` configuration to ensure the following items
+exist, adding where they do not:
+
+```bash
+# ~/.bash_profile
+export PATH=$PATH:~/bin
+export BASH_IT=$HOME/.bash_it
+export BASH_IT_THEME="bobby"
+export EDITOR="/usr/bin/emacs"
+export GIT_EDITOR="/usr/bin/emacs"
+
+source $BASH_IT/bash_it.sh
+source ~/.bashrc
+```
+
+### Enable infra tools
+
+Now enable the infra tools by installing the plugin and enabling it:
+
+```
+$ wget -O ~/.bash_it/custom/infra.plugin.bash URL
+$ ln -s ~/.bash_it/custom/infra.plugin.bash ~/.bash_it/plugins/enabled/infra.plugin.bash
+```
+
+and append `infra_commands_enable` to the `~/.bashrc` file:
+
+```
+echo "infra_commands_enable" >> ~/.bashrc
+```
+
+### Load tools
+
+Local configuration is now complete. The terminal may be closed, and all new
+instances will have the tooling auto-enabled. If you would like to load the
+tools directly into the existing instance, force the profile to reload:
+
+```
+$ source ~/.bash_profile
+```
+
+## Usage
+
+Navigate to your local infrastructure repository.
+
+### Initialization
+
+To start, the directory must be initialized:
+
+```
+$ infra-config
+```
+
+This will create a custom `.envrc` file for `direnv` to use. If a `.envrc`
+file already exists the command will print an error and exit. If the file
+already exists, back it up and remove the `.envrc` file. Then run the command
+again.
+
+### Configuration
+
+#### Accounts
+
+Open the newly created `.envrc` file and locate the "User edit section" at
+the top of the file. This is pre-filled with AWS as the provider and us-west-1
+as the current region. It is important to note the `PROVIDER_ACCOUNT` value
+which will be set to `default`.
+
+In the next section (AWS) values exist for the `DEFAULT_ACCESS_KEY_ID` and the
+`DEFAULT_SECRET_ACCESS_KEY`. The prefix used `DEFAULT` matches the name set
+within the `PROVIDER_ACCOUNT` environment variable. This allows defining
+multiple "accounts" and storing their credentials by changing the prefix.
+For example, lets assume two accounts are currently in use: `default` and
+`production`. To support these two accounts, values are defined for each:
+
+```bash
+export DEFAULT_ACCESS_KEY_ID="CHANGEME_DEFAULT"
+export DEFAULT_SECRET_ACCESS_KEY="CHANGEME_DEFAULT"
+export PRODUCTION_ACCESS_KEY_ID="CHANGEME_PRODUCTION"
+export PRODUCTION_SECRET_ACCESS_KEY="CHANGEME_PRODUCTION"
+```
+
+#### Chef
+
+The infra tools provide helpers for Chef. Settings for these are located
+within the "Chef settings" section.
+
+* encrypted secret path
+* client key
+*
+
+### Usage
+
+After completion of configuration, the prompt will update itself to reflect
+the current state of the configuration. It will now provide a visual outputs
+for:
+
+* Current provider
+* Current account
+* Current region
+* Encrypted data bag status (enabled/disabled)
+
+### Commands
+
+* `infra-provider ARG` switch to provider named `ARG`
+* `infra-acct ARG` switch to account named `ARG`
+* `infra-region ARG` switch to region named `ARG`
+* `infra-crypt` toggle encrypted data bag on/off
---
+++ infra.plugin.bash
@@ -0,0 +1,196 @@
+cite about-plugin
+about-plugin 'infra settings helpers'
+
+## Add some simple helpers to get us rolling
+function direnv_rc {
+    direnv status | grep "Found RC path" | cut -d ' ' -f 4
+}
+
+function direnv_fload {
+    direnv allow
+    direnv reload
+}
+
+function current_infra_acct {
+    echo ${PROVIDER_ACCOUNT:UNSET}
+}
+
+function current_infra_region {
+    echo ${PROVIDER_REGION:UNSET}
+}
+
+function current_infra_provider {
+    echo ${PROVIDER:UNSET}
+}
+
+function extract_infra_value {
+    var="${PROVIDER_ACCOUNT}_${1}"
+    echo ${!var}
+}
+
+function extract_infra_value_or_default {
+    value=$(extract_infra_value ${1})
+    if [[ value = '' ]]
+    then
+        var="${PROVIDER_ACCOUNT}_${1}"
+        echo ${!var}
+    else
+        echo ${value}
+    fi
+}
+
+## Define `infra-*` commands
+function infra-config {
+    about 'configures environment for infra'
+    group 'infra'
+
+    if [ -f .envrc ]
+    then
+        echo -e "${echo_bold_red}ERROR:${echo_reset_color} Will not overwrite existing .envrc file!"
+    else
+        cat <<EOF > .envrc
+##### START: User edit section #####
+
+# Provider settings
+export PROVIDER="aws"
+export PROVIDER_ACCOUNT="default"
+export PROVIDER_REGION="us-west-1"
+
+# AWS
+export DEFAULT_ACCESS_KEY_ID="CHANGEME"
+export DEFAULT_SECRET_ACCESS_KEY="CHANGEME"
+
+# Chef settings
+export CHEF_ENABLED="yes"
+export DEFAULT_KNIFE_USER="${USER:UNSET}"
+export DEFAULT_KNIFE_CHEF_SECRET_FILE_PATH=""
+export DEFAULT_KNIFE_CHEF_SERVER_URL=""
+export DEFAULT_KNIFE_CLIENT_KEY=""
+
+export DEFAULT_SSH_IDENTITY_FILE="CHANGEME"
+
+##### END: User edit section #####
+
+##### START: Infra configuration #####
+##### NOTE: DO NOT EDIT #####
+INFRA_PROVIDER=`echo ${PROVIDER} | tr '[:lower:]' '[:upper:]'`
+export KNIFE_CHEF_ENCRYPTED="OFF"
+
+if [[ $CHEF_ENABLED = "yes" ]]
+then
+    export KNIFE_USER=$(extract_infra_value "KNIFE_USER")
+    export KNIFE_CHEF_SECRET_FILE_PATH=$(extract_infra_value_or_default "KNIFE_CHEF_SECRET_FILE_PATH")
+    export KNIFE_CHEF_SERVER_URL=$(extract_infra_value_or_default "KNIFE_CHEF_SERVER_URL")
+    export KNIFE_CHEF_CLIENT_KEY=$(extract_infra_value_or_default "KNIFE_CHEF_CLIENT_KEY")
+    if [[ $KNIFE_CHEF_ENCRYPTED = "ON" ]]
+    then
+        export KNIFE_CHEF_SECRET_FILE="${KNIFE_CHEF_SECRET_FILE_PATH}"
+    fi
+fi
+
+if [[ $INFRA_PROVIDER = 'AWS' ]]
+then
+    export AWS_ACCESS_KEY_ID=$(extract_infra_value "ACCESS_KEY_ID")
+    export AWS_SECRET_ACCESS_KEY=$(extract_infra_value "SECRET_ACCESS_KEY")
+    export AWS_REGION="${PROVIDER_REGION}"
+    export AWS_DEFAULT_REGION="${PROVIDER_REGION}"
+elif [[ $INFRA_PROVIDER = 'OPENSTACK' ]]
+then
+elif [[ $INFRA_PROVIDER = 'RACKSPACE' ]]
+then
+else
+    echo "ERROR: Unknown provider set - ${PROVIDER}"
+fi
+
+export KNIFE_SSH_KEY=$(extract_infra_value_or_default "SSH_IDENTITY_FILE")
+
+##### END: Infra configuration #####
+EOF
+        echo "Initialized direnv!"
+        direnv_fload
+    fi
+}
+
+function infra-crypt {
+    about 'toggles chef data bag encryption'
+    group 'infra'
+
+    echo -e -n "${echo_bold_white}Data bag item encryption:${echo_reset_color} "
+    if [[ $KNIFE_CHEF_ENCRYPTED = 'ON' ]]
+    then
+        sed -i "s/export KNIFE_CHEF_ENCRYPTED=.*/export KNIFE_CHEF_ENCRYPTED=OFF/" $(direnv_rc)
+        echo -e -n "${echo_red}disabled"
+    else
+        sed -i "s/export KNIFE_CHEF_ENCRYPTED=.*/export KNIFE_CHEF_ENCRYPTED=ON/" $(direnv_rc)
+        echo -e -n "${echo_green}enabled"
+    fi
+    echo -e "${echo_reset_color}"
+    direnv_fload
+}
+
+function infra-acct {
+    about 'switch infra account'
+    param '1: account name'
+    example '$ infra-acct default'
+    group 'infra'
+
+    ACCT=${1:default}
+    echo -e "${echo_bold_white}Account changed to:${echo_reset_color} ${echo_purple}${ACCT}${echo_reset_color}"
+    sed -i "s/export PROVIDER_ACCOUNT=.*/export PROVIDER_ACCOUNT=${ACCT}/" $(direnv_rc)
+    direnv_fload
+}
+
+function infra-provider {
+    about 'switch infra provider'
+    param '1: provider name'
+    example '$ infra-provider aws'
+    group 'infra'
+
+    PROV=${1:aws}
+    echo -e "${echo_bold_white}Provider changed to:${echo_reset_color} ${echo_purple}${PROV}${echo_reset_color}"
+    sed -i "s/export PROVIDER=.*/export PROVIDER=${PROV}/" $(direnv_rc)
+    direnv_fload
+}
+
+function infra-region {
+    about 'switch infra region'
+    param '1: region name'
+    example '$ infra-region us-east-1'
+    group 'infra'
+
+    REGION=${1:unset}
+    echo -e "${echo_bold_white}Region change to:${echo_reset_color} ${echo_purple}${REGION}${echo_reset_color}"
+    sed -i "s/export PROVIDER_REGION=.*/export PROVIDER_REGION=${REGION}/" $(direnv_rc)
+    direnv_fload
+}
+
+## Display helpers
+function infra_account_display {
+    if [[ $PROVIDER != '' ]]
+    then
+        echo -e "[${yellow}$(current_infra_provider)${orange}:${yellow}$(current_infra_acct)${orange}:${yellow}$(current_infra_region)${orange}] "
+    fi
+}
+
+function infra_data_bag_crypt_display {
+    if [[ $KNIFE_CHEF_ENCRYPTED != '' ]]
+    then
+        if [[ $KNIFE_CHEF_ENCRYPTED = 'ON' ]]
+        then
+            echo -e "${red}{x} "
+        else
+            echo -e "${cyan}{-} "
+        fi
+    fi
+}
+
+# Configure prompt to display all information
+function infra_command_prompt {
+    PS1="\n${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${orange}$(infra_provider_display):$(infra_account_display)${red}$(infra_data_bag_crypt_display)${green}→${reset_color} "
+}
+
+# Enable the commands
+function infra_commands_enable {
+    eval "$(direnv hook bash)"
+    PROMPT_COMMAND="_direnv_hook;custom_prompt_command"
+}
